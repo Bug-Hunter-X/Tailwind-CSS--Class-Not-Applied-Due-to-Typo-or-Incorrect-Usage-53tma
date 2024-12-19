1bcd160b03173b40bac70e7681837e06df49324b
BugHunter-X <askzenai@gmail.com>
2024-12-19 04:18:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,9 @@
+```javascript
+// Example showing a Tailwind CSS error where a class is not applied correctly due to a typo or incorrect selector.
+<div class="bg-blue-500 p-4 text-white">
+  This div should have a blue background, but doesn't.
+</div>
+
+// In this case, the error is a typo in the class name: `bg-blue-500` is correct, not `bg-blue-5000`.
+// Another common error: forgetting to import or include Tailwind directives in your project.
+```
